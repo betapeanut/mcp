fe437f2b74f6dc927e3e21c2015303d1e9519290
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-05 09:56:47 -0500

    update

--- brave-search/install.js
+++ brave-search/install.js
@@ -1,31 +1,29 @@
-module.exports = {
-  config: {
-    darwin: "{{env.HOME}}/Library/Application Support/Claude/claude_desktop_config.json",
-    win32: "{{path.resolve(env.APPDATA, 'Claude/claude_desktop_config.json')}}",
-  },
-  pre: [{
-    env: "BRAVE_API_KEY",
-    description: "Brave search API key https://brave.com/search/api/"
-  }],
-  run: async (kernel) => {
-    return [
-      {
-        method: "json.set",
-        params: {
-          "{{self.config[platform]}}": {
-            "mcpServers.brave-search": {
-              "env": {
-                "BRAVE_API_KEY": kernel.envs.BRAVE_API_KEY
-              },
-              "command": "npx",
-              "args": [
-                "-y",
-                "@modelcontextprotocol/server-brave-search"
-              ]
-            }
+module.exports = async (kernel) => {
+  return {
+    config: {
+      darwin: "{{env.HOME}}/Library/Application Support/Claude/claude_desktop_config.json",
+      win32: "{{path.resolve(env.APPDATA, 'Claude/claude_desktop_config.json')}}",
+    },
+    pre: [{
+      env: "BRAVE_API_KEY",
+      description: "Brave search API key https://brave.com/search/api/"
+    }],
+    run: [{
+      method: "json.set",
+      params: {
+        "{{self.config[platform]}}": {
+          "mcpServers.brave-search": {
+            "env": {
+              "BRAVE_API_KEY": kernel.envs.BRAVE_API_KEY
+            },
+            "command": "npx",
+            "args": [
+              "-y",
+              "@modelcontextprotocol/server-brave-search"
+            ]
           }
         }
-      },
-    ]
+      }
+    }]
   }
 }
